1fa25ad0c60782a694a249d8f1b10126b2a7e054
Mike Travers <mt@collabrx.com>
2009-12-17 17:12:59 -0800

    fix problem with NCBI returning bad data in ambient query
    --HG--
    branch : nlistener

--- mt-utils.lisp
+++ mt-utils.lisp
@@ -36,8 +36,8 @@ NOTE: This is the canonical version!  Accept no substitutes.
          (nconc ,place
                 (list ,thing))))
 
-(defmacro pushnew-end (item place)
-  `(if (member ,item ,place)
+(defmacro pushnew-end (item place &key test)
+  `(if (member ,item ,place :test test)
      ,place
      (push-end ,item ,place)))
 
